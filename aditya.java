e43fa3782a0567dc8c907471fd30a24362c8d100
maaz gazi <maazgazi38@gmail.com>
2026-07-03 12:48:13 +0530

    new file added

---
+++ aditya.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class aditya {
+    public static void main(String[] args) {
+        
+
+        ArrayList <String> list = new ArrayList<>();
+
+        list.add("Hello");
+       list.add("12");
+       list.add("avdhoot");
+
+        String nm = list.get(0);
+
+        String km = list.get(2);
+
+        System.err.println(km);
+
+        System.out.println(nm);
+
+    }
+}
